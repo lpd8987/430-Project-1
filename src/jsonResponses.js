ede129cd598f9605437561b39cf5c55c41767b5f
lpd8987 <lpd8987@rit.edu>
2022-10-02 17:25:39 -0400

    Added GET and HEAD methods, need to update POST and Client

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,13 +1,86 @@
-const fs = require("fs");
+// const vocabList = {};
+
+const fs = require('fs');
 
 const verbs = fs.readFileSync(`${__dirname}/../data/verbs.json`);
+const nouns = fs.readFileSync(`${__dirname}/../data/nouns.json`);
+const adjectives = fs.readFileSync(`${__dirname}/../data/adjectives.json`);
+const adverbs = fs.readFileSync(`${__dirname}/../data/adverbs.json`);
 
-const getExternalJSONData = (request, response) => {
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    response.write(verbs);
-    response.end();
+// GET
+// Sends the GET response
+const getJSON = (request, response, jsonObj, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(jsonObj);
+  response.end();
 };
 
-module.exports = { 
-    getExternalJSONData,
+// Returns the list of Verbs
+const getVerbJSON = (request, response) => {
+  getJSON(request, response, verbs, 200);
+};
+
+// Returns the list of Nouns
+const getNounJSON = (request, response) => {
+  getJSON(request, response, nouns, 200);
+};
+
+// Returns the list of Adjectives
+const getAdjectiveJSON = (request, response) => {
+  getJSON(request, response, adjectives, 200);
+};
+
+// Returns the list of Adverbs
+const getAdverbJSON = (request, response) => {
+  getJSON(request, response, adverbs, 200);
+};
+
+// Sends an error object (404 - not found) if the url is not found
+const JSONNotFound = (request, response) => {
+  const jsonObj = {
+    message: 'The page you are looking for was not found',
+    id: 'notFound',
+  };
+
+  getJSON(request, response, JSON.stringify(jsonObj), 404);
+};
+
+// HEAD
+// Sends the HEAD response
+const returnJSONHead = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+// sets the status code for the HEAD (200 - Success)
+const returnListHead = (request, response) => {
+  returnJSONHead(request, response, 200);
+};
+
+// sets the status code for the HEAD (404 - Not Found)
+const headNotFound = (request, response) => {
+  returnJSONHead(request, response, 404);
+};
+
+// POST
+
+// Server Error
+const serverError = (request, response) => {
+  const jsonObj = {
+    message: 'Something went wrong with the server.',
+    id: 'serverError',
+  };
+
+  getJSON(request, response, JSON.stringify(jsonObj), 500);
+};
+
+module.exports = {
+  getVerbJSON,
+  getNounJSON,
+  getAdjectiveJSON,
+  getAdverbJSON,
+  JSONNotFound,
+  returnListHead,
+  headNotFound,
+  serverError,
 };
--- src/server.js
+++ src/server.js
@@ -1,34 +1,69 @@
-const http = require("http");
-const url = require("url");
-const query = require("querystring");
+const http = require('http');
+const url = require('url');
+// const query = require('querystring');
 
-const jsonHandler = require("./jsonResponses.js");
-const { parse } = require("path");
+const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-
 const handleGET = (request, response, parsedURL) => {
-    if(parsedURL.pathname === "/verbs"){
-        jsonHandler.getExternalJSONData(request, response);
-    }
-}
+  switch (parsedURL.pathname) {
+    case '/':
+      break;
+    case '/clientStyle.css':
+      break;
+    case '/verbs':
+      jsonHandler.getVerbJSON(request, response);
+      break;
+    case '/nouns':
+      jsonHandler.getNounJSON(request, response);
+      break;
+    case '/adjectives':
+      jsonHandler.getAdjectiveJSON(request, response);
+      break;
+    case '/adverbs':
+      jsonHandler.getAdverbJSON(request, response);
+      break;
+    default:
+      jsonHandler.JSONNotFound(request, response);
+      break;
+  }
+};
 
+const handleHEAD = (request, response, parsedURL) => {
+  switch (parsedURL.pathname) {
+    case '/verbs':
+    case '/nouns':
+    case '/adjectives':
+    case '/adverbs':
+      jsonHandler.returnListHead(request, response);
+      break;
+    default:
+      jsonHandler.headNotFound(request, response);
+      break;
+  }
+};
+
+// Route requests depending on therir
 const onRequest = (request, response) => {
-    const parsedURL = url.parse(request.url);
+  const parsedURL = url.parse(request.url);
 
-    switch(request.method) {
-     case "POST":
-        break;
-     case "HEAD":
-        break;
-     case "GET":
-     default:
-        handleGET(request, response, parsedURL);
-        break;
-    }
+  switch (request.method) {
+    case 'POST':
+      break;
+    case 'HEAD':
+      handleHEAD(request, response, parsedURL);
+      break;
+    case 'GET':
+      handleGET(request, response, parsedURL);
+      break;
+    default:
+      // user attempts an operation other than POST, HEAD, or GET
+      jsonHandler.serverError(request, response);
+      break;
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on port 127.0.0.1:${port}`);
+  console.log(`Listening on port 127.0.0.1:${port}`);
 });
